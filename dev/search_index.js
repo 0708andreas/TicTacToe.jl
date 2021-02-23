a81558db025c09c64e5304aa19e5928fbe9d5a7c
Documenter.jl <documenter@juliadocs.github.io>
2021-02-23 09:50:05 +0000

    build based on cc3272d

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TicTacToe","category":"page"},{"location":"#TicTacToe","page":"Home","title":"TicTacToe","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TicTacToe]","category":"page"}]
+}
